de06f5757f44bc798f7cbc13c32a7bc8dd38bcfb
Ivan-Toshiba <ivp.vankata@gmail.com>
2024-08-04 14:17:46 +0300

    Removed not used css file and updated index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import WidgetList from '../components/WidgetList';
-import styles from '../styles/Home.module.scss';
 
 const Home = () => {
 	return (
-		<div className={styles.container}>
+		<div>
 			<WidgetList />
 		</div>
 	);
