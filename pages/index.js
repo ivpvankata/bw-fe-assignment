8434ec165032b888a13dc62d4f5bfed39e73b5f4
Ivan-Toshiba <ivp.vankata@gmail.com>
2024-08-04 14:54:21 +0300

    Add _document.js in order to modify the <head>

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 import WidgetList from '../components/WidgetList';
 
 const Home = () => {
 	return (
-		<div>
-			<WidgetList />
-		</div>
+		<>
+			<Head>
+				<title>Best Casinos 2024</title>
+				<meta name="description" content="Find the best casinos in 2024. Play responsibly and enjoy great bonuses." />
+			</Head>
+			<div>
+				<WidgetList />
+			</div>
+		</>
 	);
 };
 
