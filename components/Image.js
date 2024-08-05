461f1b8f301b09999565f99c03d1eebc99035401
Ivan-Toshiba <ivp.vankata@gmail.com>
2024-08-05 08:50:17 +0300

    Add WCAG improvements

--- components/Image.js
+++ components/Image.js
@@ -4,8 +4,8 @@ import styles from '../styles/Image.module.scss';
 const Image = ({ backgroundColor, titleUrl, imageSrc, title }) => {
 	return (
 		<div className={styles.widgetImage}>
-			<a href={titleUrl} style={{ backgroundColor }}>
-				<img src={imageSrc} alt={title} className={styles.widgetImg} />
+			<a href={titleUrl} aria-label={`Link to ${title}`} style={{ backgroundColor }}>
+				<img src={imageSrc} alt={`Logo of ${title}`} className={styles.widgetImg} />
 			</a>
 		</div>
 	);
--- components/Rating.js
+++ components/Rating.js
@@ -3,11 +3,15 @@ import styles from '../styles/Rating.module.scss';
 
 const Rating = ({ rating }) => {
 	return (
-		<ul className={styles.widgetStarsRating} aria-label="Star rating">
-			{[...Array(5)].map((_, i) => (
-				<li key={i} className={`${styles.star} ${i < rating ? styles.filled : styles.empty}`} aria-hidden="true"></li>
-			))}
-		</ul>
+		<div>
+			<span className="sr-only">{`Rated ${rating} out of 5 stars`}</span>
+
+			<ul className={styles.widgetStarsRating} aria-label="Star rating">
+				{[...Array(5)].map((_, i) => (
+					<li key={i} className={`${styles.star} ${i < rating ? styles.filled : styles.empty}`} aria-hidden="true"></li>
+				))}
+			</ul>
+		</div>
 	);
 };
 
